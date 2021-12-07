4f333ab6885dd6a57fb16109431029a8621a2343
SujalShah3234 <shahc9437@gmail.com>
2021-12-07 17:03:25 +0530

    Mass converter added

---
+++ app/src/main/java/com/sujal/unitconverter/FrequencyActivity.java
@@ -0,0 +1,14 @@
+package com.sujal.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrequencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frequency);
+    }
+}
--- app/src/main/java/com/sujal/unitconverter/MainActivity.java
+++ app/src/main/java/com/sujal/unitconverter/MainActivity.java
@@ -23,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
             action_bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_500)));
         }
 
-        CardView area_card, length_card, temperature_card, speed_card;
+        CardView area_card, length_card, temperature_card, speed_card, frequency_card, mass_card;
 
         area_card = findViewById(R.id.cardView_area);
         length_card = findViewById(R.id.cardView_length);
         temperature_card = findViewById(R.id.cardView_temperature);
         speed_card = findViewById(R.id.cardView_speed);
+        frequency_card = findViewById(R.id.cardView_frequency);
+        mass_card = findViewById(R.id.cardView_mass);
 
         area_card.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +57,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SpeedActivity.class));
+            }
+        });
+
+        frequency_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, FrequencyActivity.class));
+            }
+        });
+
+        mass_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MassActivity.class));
             }
         });
     }
